cebb45703be0fc994547f68dea6b7486768bf621
Vault Thirteen <54213347+vault-thirteen@users.noreply.github.com>
2025-11-02 10:54:06 +0300

    rework

--- cmd/SFRODB/client/start-client.bat
+++
@@ -1,1 +0,0 @@
-client.exe localhost 2001 3001
--- pkg/SFRODB/common/error/type/type.go
+++
@@ -1,9 +0,0 @@
-package cet
-
-// ErrorType is a common error type.
-type ErrorType byte
-
-const (
-	ErrorTypeServer = ErrorType(1)
-	ErrorTypeClient = ErrorType(2)
-)
